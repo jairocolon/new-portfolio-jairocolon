fb831fc39475d1f0dbbf811c989a2e24e83b7c7e
Jairo Colón <83477127+jairocolon@users.noreply.github.com>
2022-08-10 15:18:23 -0500

    Add Contact Form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import About from './components/About';
+import Contact from './components/Contact';
 import Home from './components/Home';
 import NavBar from './components/NavBar';
 import Projects from './components/Projects';
@@ -12,6 +13,7 @@ function App() {
 			<About />
 			<Skills />
 			<Projects />
+			<Contact />
 		</div>
 	);
 }
