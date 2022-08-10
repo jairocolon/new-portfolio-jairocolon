0017b6ba67e1a988c5abcee60f29db0ae9a6ebb5
Jairo Colón <83477127+jairocolon@users.noreply.github.com>
2022-08-10 15:53:34 -0500

    Update some icons

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,7 +3,7 @@ import HTML from '../assets/html.png';
 import CSS from '../assets/css.png';
 import JavaScript from '../assets/javascript.png';
 import ReactImg from '../assets/react.png';
-import Node from '../assets/node.png';
+import Redux from '../assets/redux.png';
 import Python from '../assets/python.png';
 import GitHub from '../assets/github.png';
 import Tailwind from '../assets/tailwind.png';
@@ -38,8 +38,12 @@ const Skills = () => {
 						<p className="my-4">REACT</p>
 					</div>
 					<div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+						<img className="w-20 mx-auto" src={Redux} alt="HTML icon" />
+						<p className="my-4">REDUX</p>
+					</div>
+					<div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
 						<img className="w-20 mx-auto" src={Python} alt="HTML icon" />
-						<p className="my-4">PAYTHON</p>
+						<p className="my-4">PYTHON</p>
 					</div>
 					<div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
 						<img className="w-20 mx-auto" src={Tailwind} alt="HTML icon" />
@@ -48,10 +52,6 @@ const Skills = () => {
 					<div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
 						<img className="w-20 mx-auto" src={GitHub} alt="HTML icon" />
 						<p className="my-4">GITHUB</p>
-					</div>
-					<div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-						<img className="w-20 mx-auto" src={Node} alt="HTML icon" />
-						<p className="my-4">NODE JS</p>
 					</div>
 				</div>
 			</div>
