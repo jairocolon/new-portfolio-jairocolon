4dda1fd025e382ce7b0ced40bebbb385cc19849c
Jairo Colón <83477127+jairocolon@users.noreply.github.com>
2022-08-22 09:02:10 -0500

    Update Projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import CajeMovies from '../assets/cajemovies.png';
 import CataHernandez from '../assets/catahernandez.png';
+import Generador_de_Contraseñas from '../assets/generador_de_contraseñas.png';
+import Paises_Del_mundo from '../assets/paises_del_mundo.png';
+import Jaico_Digital from '../assets/jaico_digital.png';
+import Creativo_Digital from '../assets/creativo_digital.png';
 
 const Projects = () => {
 	return (
@@ -83,21 +87,21 @@ const Projects = () => {
 					</div>
 					{/* Grid Item */}
 					<div
-						style={{ backgroundImage: `url(${CajeMovies})` }}
+						style={{ backgroundImage: `url(${Generador_de_Contraseñas})` }}
 						className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
 					>
 						{/* Hover Effects */}
 						<div className="opacity-0 group-hover:opacity-100">
 							<span className="text-2xl font-bold text-white tracking-wider">
-								React JS Application
+								Django Application
 							</span>
 							<div className="pt-8 text-center">
 								<a href="/" target="_blank" rel="noopener noreferrer">
 									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-										Demo
+										Demo not available
 									</button>
 								</a>
-								<a href="/" target="_blank" rel="noopener noreferrer">
+								<a href="https://github.com/jairocolon/django-generador-contrase-as" target="_blank" rel="noopener noreferrer">
 									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
 										Code
 									</button>
@@ -106,21 +110,21 @@ const Projects = () => {
 						</div>
 					</div>
 					<div
-						style={{ backgroundImage: `url(${CataHernandez})` }}
+						style={{ backgroundImage: `url(${Paises_Del_mundo})` }}
 						className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
 					>
 						{/* Hover Effects */}
 						<div className="opacity-0 group-hover:opacity-100">
 							<span className="text-2xl font-bold text-white tracking-wider">
-								React JS Application
+								React, Node, JS
 							</span>
 							<div className="pt-8 text-center">
-								<a href="/" target="_blank" rel="noopener noreferrer">
+								<a href="https://pi-countries-jairo-col-n.vercel.app/" target="_blank" rel="noopener noreferrer">
 									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
 										Demo
 									</button>
 								</a>
-								<a href="/" target="_blank" rel="noopener noreferrer">
+								<a href="https://github.com/jairocolon/PI-Countries-Jairo-Col-n" target="_blank" rel="noopener noreferrer">
 									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
 										Code
 									</button>
@@ -130,16 +134,16 @@ const Projects = () => {
 					</div>
 					{/* Grid Item */}
 					<div
-						style={{ backgroundImage: `url(${CajeMovies})` }}
+						style={{ backgroundImage: `url(${Jaico_Digital})` }}
 						className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
 					>
 						{/* Hover Effects */}
 						<div className="opacity-0 group-hover:opacity-100">
 							<span className="text-2xl font-bold text-white tracking-wider">
-								React JS Application
+							WordPress Application
 							</span>
 							<div className="pt-8 text-center">
-								<a href="/" target="_blank" rel="noopener noreferrer">
+								<a href="https://jaicodigital.com/" target="_blank" rel="noopener noreferrer">
 									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
 										Demo
 									</button>
@@ -153,13 +157,13 @@ const Projects = () => {
 						</div>
 					</div>
 					<div
-						style={{ backgroundImage: `url(${CataHernandez})` }}
+						style={{ backgroundImage: `url(${Creativo_Digital})` }}
 						className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
 					>
 						{/* Hover Effects */}
 						<div className="opacity-0 group-hover:opacity-100">
 							<span className="text-2xl font-bold text-white tracking-wider">
-								React JS Application
+								WordPress Application
 							</span>
 							<div className="pt-8 text-center">
 								<a href="/" target="_blank" rel="noopener noreferrer">
@@ -167,7 +171,7 @@ const Projects = () => {
 										Demo
 									</button>
 								</a>
-								<a href="/" target="_blank" rel="noopener noreferrer">
+								<a href="https://creativodigital.com.co/" target="_blank" rel="noopener noreferrer">
 									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
 										Code
 									</button>
